5e782be9c0f34b945d16588d64b4b196783d94e7
Evan Ossege <evanossege@gmail.com>
2023-02-20 20:22:45 -0500

    Initial Commit

---
+++ ReceptionistApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ReceptionistApp
+//
+//  Created by Evan Ossege on 2/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ReceptionistApp/ReceptionistAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReceptionistAppApp.swift
+//  ReceptionistApp
+//
+//  Created by Evan Ossege on 2/20/23.
+//
+
+import SwiftUI
+
+@main
+struct ReceptionistAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
